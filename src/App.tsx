8ce1ca9aca743756a75cc94179a7fc4501d63fca
Ragesh <ck.ragesh@gmail.com>
2024-08-21 19:30:17 +0100

    commit changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactaa
         </a>
       </header>
     </div>
